8a0ca060306e10a41039417a9dd0efaa5855e6e8
Jack Roche <52017135+jackr271@users.noreply.github.com>
2024-02-08 20:51:12 -0500

    feat: implement playRound function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// no input, returns 'rock' 'paper' or 'scissors' by random chance
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3) // selects a random integer between 0 and 2
     switch (num) {
@@ -6,5 +7,30 @@ function getComputerChoice() {
         case 1: return 'paper';
 
         case 2: return 'scissors';
+    }
+}
+
+// inputs player's and computer's choices, returns win, lose, or tie result as string
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors')
+            return `Win! ${playerSelection} beats ${computerSelection}`;
+        else
+            return `Loss! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock')
+            return `Win! ${playerSelection} beats ${computerSelection}`;
+        else
+            return `Loss! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        if (computerSelection === 'paper')
+            return `Win! ${playerSelection} beats ${computerSelection}`;
+        else
+            return `Loss! ${computerSelection} beats ${playerSelection}`;
     }
 }
